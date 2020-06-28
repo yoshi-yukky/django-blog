bc67256119c64c0408f9d1366de5c5d33a42ef1a
yoshikado\yukimasa <sioazinori@hotmail.co.jp>
2020-06-28 21:53:45 +0900

    settings.pyを編集
    templates ->  templateへ
    staticのフォルダパスを追記

--- django/project/config/settings.py
+++ django/project/config/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'template'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS  = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
