60120736093a8e9f82c27863f153b2331e1a4c8b
yoshikado\yukimasa <sioazinori@hotmail.co.jp>
2020-06-28 21:54:04 +0900

    staticのファイルを読み込むように編集

--- django/project/config/urls.py
+++ django/project/config/urls.py
@@ -22,9 +22,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
     path('', include('blog.urls')),
-]
-
-urlpatterns += static(
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+) + static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
