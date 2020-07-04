272a98e98a466aad6a67942b36aff6c11bd1b590
yoshikado\yukimasa <sioazinori@hotmail.co.jp>
2020-07-05 00:52:47 +0900

    Categoryを追加

--- django/project/blog/admin.py
+++ django/project/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 
-from .models import Post
+from .models import Post, Category
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Category)
--- django/project/blog/models.py
+++ django/project/blog/models.py
@@ -3,9 +3,19 @@ from django.utils.safestring import mark_safe
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT
+    )
     content = MarkdownxField()
     excerpt = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
