a1adc83966c5abb52a8c20131089d8becae9fb6f
sioazinori <sioazinori@hotmail.co.jp>
2021-05-26 23:03:14 +0900

    移動

--- django/project/common/views.py
+++ django/project/common/views.py
@@ -1,10 +1,12 @@
 from django.views import generic
+from common.decorators import disable_side_bar
 
 
+@disable_side_bar
 class ProfileView(generic.TemplateView):
     template_name = "profile.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["disable_category"] = True
+        context["side_menu"] = True
         return context
